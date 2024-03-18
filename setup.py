a5f6a6edfa4ef6a5956329ec5161d981013f19ce
Alvaro Fernandez Garcia <alvarofdzg99@gmail.com>
2024-03-18 13:24:17 -0400

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+
+def main(paths:list) -> None:
+
+    # Creating output folders:
+    for path in paths:
+        cur_path = os.path.join(
+            path,
+            'output_data'
+        )
+        if not os.path.exists(cur_path):
+            os.mkdir(cur_path)
+        
+
+if __name__ == '__main__':
+    
+    paths = [
+        '1_visualize_different_matrices',
+        '2_spectral_decomposition',
+        '3_singular_value_decomposition'
+    ]
+    main(paths=paths)
